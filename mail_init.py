f0c4a1337fe317c890834459fc444ce48bbb7e74
midoks <midoks@163.com>
2022-09-22 22:31:18 +0800

    Update mail_init.py

--- mail_init.py
+++ mail_init.py
@@ -72,6 +72,8 @@ class mail_init:
 
 
 if __name__ == "__main__":
-    # python3 /www/server/mdserver-web/plugins/mail/mail_init.py
-    t = mail_init.mail_init().check_postfix_ver()
+    '''
+    cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/mail/mail_init.py
+    '''
+    t = mail_init().check_postfix_ver()
     print(t)
